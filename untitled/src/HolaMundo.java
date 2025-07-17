39cc9871f566bb366c26b6a2ec2187a9857ea386
JoanGarciaBild <joan.garcia@bild.com.mx>
2025-07-17 14:17:50 -0700

    Ejemplo Variables

--- untitled/src/HolaMundo.java
+++ untitled/src/HolaMundo.java
@@ -1,5 +1,18 @@
 public class HolaMundo {
     public static void main(String[] args) {
-        System.out.println("Hola Mundo");
+        String saludar = "Hola Mundo";
+        System.out.println(saludar);
+        System.out.println("saludar.toUpperCase() = " + saludar.toUpperCase());
+        
+        int numero = 10;
+        boolean valor = true;
+        int numero2 = 5;
+        if(valor){
+            System.out.println("numero = " + numero);
+            numero2 = 10;
+        }
+        System.out.println("numero2 = " + numero2);
+
+        var numero3 = "15";
     }
 }
