b15328a6c41bf91c7f86daf0fdf536b9595ef65a
JoanGarciaBild <joan.garcia@bild.com.mx>
2025-07-21 10:38:44 -0700

    Obtener la extensión de un archivo

---
+++ HolaMundo/src/EjemploStringExtensionArchivo.java
@@ -0,0 +1,11 @@
+public class EjemploStringExtensionArchivo {
+    public static void main(String[] args) {
+
+        String archivo = "alguna_imagen.jpeg";
+        int i = archivo.lastIndexOf(".");
+        System.out.println("archivo.length() = " + archivo.length());
+        System.out.println("archivo. = " + archivo.substring(i+1));
+
+
+    }
+}
