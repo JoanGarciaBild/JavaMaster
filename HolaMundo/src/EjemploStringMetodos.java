ca4e5fd0fe8688519d39681f5af066dc7b1c66ba
JoanGarciaBild <joan.garcia@bild.com.mx>
2025-07-21 10:26:58 -0700

    Métodos importantes del objeto String parte 1

--- HolaMundo/src/EjemploStringMetodos.java
+++ HolaMundo/src/EjemploStringMetodos.java
@@ -3,6 +3,18 @@ public class EjemploStringMetodos {
 
         String nombre = "Joan";
         System.out.println("nombre.length() = " + nombre.length());
+        System.out.println("nombre.toUpperCase() = " + nombre.toUpperCase());
+        System.out.println("nombre.toLowerCase() = " + nombre.toLowerCase());
+        System.out.println("nombre.equals(\"Joan\") = " + nombre.equals("Joan"));
+        System.out.println("nombre.equals(\"Joan\") = " + nombre.equals("joan"));
+        System.out.println("nombre.equalsIgnoreCase(\"joan\") = " + nombre.equalsIgnoreCase("joan"));
+        System.out.println("nombre.compareTo(\"Joan\") = " + nombre.compareTo("Joan"));
+        System.out.println("nombre.compareTo(\"Juancito\") = " + nombre.compareTo("Juancito"));
+        System.out.println("nombre.charAt(0) = " + nombre.charAt(0));
+        System.out.println("nombre.charAt(3) = " + nombre.charAt(3));
+        System.out.println("nombre.charAt(nombre.length()-1) = " + nombre.charAt(nombre.length()-1));
+        System.out.println("nombre.substring(1) = " + nombre.substring(1));
+        System.out.println("nombre.substring(1, 4) = " + nombre.substring(1, 4));
         
 
     }
