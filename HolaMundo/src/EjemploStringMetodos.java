c2d1c10323b87f76f05c2c5bb0a9e0b7fa03c7c5
JoanGarciaBild <joan.garcia@bild.com.mx>
2025-07-21 10:02:35 -0700

    String Methods

---
+++ HolaMundo/src/EjemploStringMetodos.java
@@ -0,0 +1,9 @@
+public class EjemploStringMetodos {
+    public static void main(String[] args) {
+
+        String nombre = "Joan";
+        System.out.println("nombre.length() = " + nombre.length());
+        
+
+    }
+}
