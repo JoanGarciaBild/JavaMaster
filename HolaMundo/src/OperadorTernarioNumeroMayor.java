90d31db156fc840ca2bd9f5d0dacf687c74f5686
JoanGarciaBild <joan.garcia@bild.com.mx>
2025-07-23 11:51:54 -0700

    Operador Instanceof (Tipo de)

--- HolaMundo/src/OperadorTernarioNumeroMayor.java
+++ HolaMundo/src/OperadorTernarioNumeroMayor.java
@@ -1,4 +1,30 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class OperadorTernarioNumeroMayor {
+
+    public static void main(String[] args) {
+
+        int max = 0;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese un número");
+        int num1 = scanner.nextInt();
+
+        System.out.println("Ingrese un segundo número");
+        int num2 = scanner.nextInt();
+
+        System.out.println("Ingrese un tercer número");
+        int num3 = scanner.nextInt();
+
+        System.out.println("Ingrese un cuarto número");
+        int num4 = scanner.nextInt();
+
+        max = (num1 > num2) ? num1: num2;
+        max = (max > num3) ? max : num3;
+        max = (max > num4) ? max : num4;
+
+        System.out.println("El número mayor es: " + max);
+
+    }
+
 }
