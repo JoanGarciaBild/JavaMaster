41923812428edb16f0a525e41b68f6ba738ba200
JoanGarciaBild <joan.garcia@bild.com.mx>
2025-07-24 10:53:00 -0700

    Etiquetas en las sentencias for y while

--- HolaMundo/src/SentenciasBucleEtiquetas.java
+++ HolaMundo/src/SentenciasBucleEtiquetas.java
@@ -23,6 +23,21 @@ public class SentenciasBucleEtiquetas {
                 System.out.print("[i = " + i + ", j = " + j + "],");
             }
         }
+
+        bucle:
+        for(int i = 1; i <= 7; i++){
+            int j = 1;
+            while( j <= 8){
+                if(i == 6 || i == 7){
+                    System.out.println("Dia " + i + ": descanso de fin de semana!");
+                    continue bucle;
+                }
+                System.out.println("Dia " + i + ", trabajando a las " + j + "hrs.");
+                i++;
+            }
+        }
+
+
     }
     
 }
