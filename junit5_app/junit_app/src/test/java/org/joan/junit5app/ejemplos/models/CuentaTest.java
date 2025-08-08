ad899f0a31d668ea4becd6efeebf8ce11ea00236
JoanGarciaBild <joan.garcia@bild.com.mx>
2025-08-08 14:26:19 -0700

    Escribiendo test para el saldo

---
+++ junit5_app/junit_app/src/test/java/org/joan/junit5app/ejemplos/models/CuentaTest.java
@@ -0,0 +1,28 @@
+package org.joan.junit5app.ejemplos.models;
+
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CuentaTest {
+
+    @Test
+    void testNombreCuenta() {
+        Cuenta cuenta = new Cuenta("Joan", new BigDecimal("1000.12345"));
+        //cuenta.setPersona("Joan");
+        String esperado = "Joan";
+        String real = cuenta.getPersona();
+        assertEquals(esperado, real);
+        assertTrue(real.equals("Joan"));
+    }
+    
+    @Test
+    void testSaldoCuenta(){
+        Cuenta cuenta = new Cuenta("Andres", new BigDecimal("1000.12345"));
+        assertEquals(1000.12345, cuenta.getSaldo().doubleValue());
+        assertFalse(cuenta.getSaldo().compareTo(BigDecimal.ZERO) < 0);
+        assertTrue(cuenta.getSaldo().compareTo(BigDecimal.ZERO) > 0);
+    }
+}
