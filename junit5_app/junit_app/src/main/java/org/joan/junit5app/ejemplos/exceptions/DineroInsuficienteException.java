ee7d5aee685a6a8c4f59c39f0c2432900d3fa136
JoanGarciaBild <joan.garcia@bild.com.mx>
2025-08-11 12:22:49 -0700

    Excepciones con JUnit

--- junit5_app/junit_app/src/main/java/org/joan/junit5app/ejemplos/exceptions/DineroInsuficienteException.java
+++ junit5_app/junit_app/src/main/java/org/joan/junit5app/ejemplos/exceptions/DineroInsuficienteException.java
@@ -1,4 +1,9 @@
 package org.joan.junit5app.ejemplos.exceptions;
 
 public class DineroInsuficienteException extends RuntimeException{
+
+    public DineroInsuficienteException(String message){
+        super(message);
+    }
+
 }
