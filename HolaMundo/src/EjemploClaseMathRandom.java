670024452e43b4be5f349398c7248fe1571b193b
JoanGarciaBild <joan.garcia@bild.com.mx>
2025-09-17 09:56:59 -0700

    Math.random -> probando el método random de la clase math

---
+++ HolaMundo/src/EjemploClaseMathRandom.java
@@ -0,0 +1,15 @@
+public class EjemploClaseMathRandom {
+
+    public static void main(String[] args) {
+
+        String[] colores = {"Azul", "Amarillo", "Rojo", "Verde", "Blanco", "Negro"};
+        double random = Math.random();
+        System.out.println("random = " + random);
+        random *= colores.length;
+        System.out.println("random = " + random);
+        random = Math.floor(random);
+        System.out.println("random = " + random);
+        System.out.println("colores = " + colores[(int)random]);
+    }
+    
+}
