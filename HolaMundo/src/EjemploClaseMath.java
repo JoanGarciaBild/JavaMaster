70158e67e58a2eef2eb0c900e091a97312368b5e
JoanGarciaBild <joan.garcia@bild.com.mx>
2025-09-17 09:37:29 -0700

    La Clase math, ceil, floor, round, PI, max, min

---
+++ HolaMundo/src/EjemploClaseMath.java
@@ -0,0 +1,28 @@
+public class EjemploClaseMath {
+
+    public static void main(String[] args) {
+        
+        int absoluto = Math.abs(-3);
+        System.out.println("absoluto = " + absoluto);
+
+        absoluto = Math.abs(3);
+        System.out.println("absoluto = " + absoluto);
+
+        double max = Math.max(3.5, 1.2);
+        System.out.println("max = " + max);
+        
+        double min = Math.min(3.5, 1.2);
+        System.out.println("min = " + min);
+
+        double techo = Math.ceil(3.5);
+        System.out.println("techo = " + techo);
+        
+        double suelo = Math.floor(3.5);
+        System.out.println("suelo = " + suelo);
+
+        long redondear = Math.round(Math.PI);
+        System.out.println("redondear = " + redondear);
+        
+    }
+    
+}
