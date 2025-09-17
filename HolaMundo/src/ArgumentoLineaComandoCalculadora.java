7b1da5125d397260cb36ce1bfc01a19ca053cdae
JoanGarciaBild <joan.garcia@bild.com.mx>
2025-09-17 15:57:50 -0700

    parametros por linea de comandos

--- HolaMundo/src/ArgumentoLineaComandoCalculadora.java
+++ HolaMundo/src/ArgumentoLineaComandoCalculadora.java
@@ -1,4 +1,35 @@
-package PACKAGE_NAME;
-
 public class ArgumentoLineaComandoCalculadora {
+
+    public static void main(String[] args) {
+        if(args.length != 3){
+            System.out.println("Por favor ingresar una operación (suma, resta, div, o multi) y dos números enteros");
+            System.exit(-1);
+        }
+        String operacion = args[0];
+        int a = Integer.parseInt(args[1]);
+        int b = Integer.parseInt(args[2]);
+        double resultado = 0.0;
+        switch (operacion){
+            case "suma":
+                resultado = a+b;
+                break;
+            case "resta":
+                resultado = a-b;
+                break;
+            case "multi":
+                resultado = a*b;
+                break;
+            case "div":
+                if(b == 0){
+                    System.out.println("No puede dividirse entre 0");
+                    System.exit(-1);
+                }
+                resultado = (double)a/b;
+                break;
+            default:
+                resultado = a+b;
+        }
+        System.out.println("resultado = " + resultado);
+    }
+
 }
