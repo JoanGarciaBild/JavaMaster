9f8aef0f3972116ad4ab38be1be7cfa6c512c712
JoanGarciaBild <joan.garcia@bild.com.mx>
2025-09-17 15:48:19 -0700

    Arreglos enteros

---
+++ Arreglos/src/EjemploArreglos.java
@@ -0,0 +1,27 @@
+public class EjemploArreglos {
+
+    public static void main(String[] args) {
+
+        int[] numeros = new int[4];
+
+        numeros[0] = 1;
+        numeros[1] = Integer.valueOf("2");
+        numeros[2] = 3;
+        numeros[3] = 4;
+        //numeros[4] = 5; ArrayIndexOutOfBoundsException
+
+        int i = numeros[0];
+        int j = numeros[1];
+        int k = numeros[2];
+        int l = numeros[numeros.length-1];
+
+        System.out.println("i = " + i);
+        System.out.println("j = " + j);
+        System.out.println("k = " + k);
+        System.out.println("l = " + l);
+
+
+
+    }
+
+}
